cb34330d2db498058eeab61d73d2af765ab984a5
mathiconical <netinho340@gmail.com>
2022-09-22 20:25:19 -0300

    main skel and constant file

---
+++ src/Constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace GAEN {
+	namespace SCREEN {
+		//! pixels are 32/32
+		constexpr int WIDTH = 768;
+		constexpr int HEIGHT = 608;
+	};
+};
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "Constants.hpp"
+
+int main(int argc, char* args[]){
+	//! instance game object
+
+	//! initialize game
+
+	//! main loop
+	//! 	process input
+	//! 	update
+	//! 	render
+	
+	//! destroy/clean memory
+	return 0;
+}
